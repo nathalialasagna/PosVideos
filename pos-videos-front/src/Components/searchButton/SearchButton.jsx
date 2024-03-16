11f1205d9f48766dff9060c9ddb1fb92004bf386
Vinícius Falcão <andrefalcaov8@gmail.com.br>
2024-03-16 16:31:13 -0300

    Aidicona comportamento de nome do arquivo

--- pos-videos-front/src/Components/searchButton/SearchButton.jsx
+++ pos-videos-front/src/Components/searchButton/SearchButton.jsx
@@ -1,11 +1,12 @@
-import { SearchBar_Div, SearchBar_Button } from "./style";
+import { SearchBar_Div, SearchBar_Button, SearchBar_Input } from "./style";
 import Search from "../../Icons/search"
 
-export default function SearchButton() {
+export default function SearchButton(props) {
     return (
-        <SearchBar_Div>
-          Procurar Vídeo
-          <Search fill={"#828282"} height={"4vh"}/>
+        <SearchBar_Div htmlFor="img">
+            Procurar Vídeo
+            <Search fill={"#828282"} height={"4vh"} />
+            <SearchBar_Input type="file" accept="video/*" id="img" onChange={props.clickAction}/>
         </SearchBar_Div>
     );
   }
