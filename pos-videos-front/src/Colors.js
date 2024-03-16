8bdaf5af7fce86ddc22ad69280cd24185462796f
Vinícius Falcão <andrefalcaov8@gmail.com.br>
2024-03-16 12:58:05 -0300

    Adiciona primeiras configurações e icones

---
+++ pos-videos-front/src/Colors.js
@@ -0,0 +1,19 @@
+
+
+export const Colors = {
+    base : {
+        backgoundColor: "#61b7e1",
+        fontColor: "#fff",
+        informationColor: "#000",
+        shadowIformationColor: "#315b71",
+        InputColor: "#d9d9d9"
+    },
+    decorative : {
+        blueLight: "#4887a6",
+        blueDark: "#27495a",
+        orangeLight: "#e6a780",
+        orangeDark: "#e09160",
+        yellowLight: "#e0b860",
+        yellowDark: "#c5a358",
+    }
+}
